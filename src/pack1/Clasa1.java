0e5dd89bb1fbefb1a590ed5d20d814cd5b992172
balanviorel <balanviorel2@gmail.com>
2025-06-12 18:15:02 +0300

    second commit

---
+++ src/pack1/Clasa1.java
@@ -0,0 +1,17 @@
+package pack1;
+
+public class Clasa1 {
+    // variabile
+
+    public static int number = 25;
+
+    // metode
+
+    public static void some(){
+        System.out.println("some method body");
+
+    }
+    public void some2() {
+        System.out.println("some2 method body");
+    }
+}
---
+++ src/pack2/ClasaExecutorie.java
@@ -0,0 +1,11 @@
+package pack2;
+
+import pack1.Clasa1;
+
+public class ClasaExecutorie {
+    public static void main(String[] args) {
+        Clasa1 obiect = new Clasa1();
+
+        System.out.println(Clasa1.number);
+    }
+}
